66fb1dd29c293e44b14d739f6a9084fa76e92520
Yakir Uzan <yakiruzangreen@gmail.com>
2025-09-01 11:32:46 +0300

    add mapping

---
+++ app/napping.py
@@ -0,0 +1,10 @@
+mapping = {
+    "mappings": {
+        "properties": {
+            "TweetID": {"type": "keyword"},
+            "CreateDate": {"type": "date"},
+            "Antisemitic": {"type": "boolean"},
+            "text": {"type": "text"}
+        }
+    }
+}
